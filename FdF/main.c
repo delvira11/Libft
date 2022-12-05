1e8502f619ce2ffb9578b81f4722ca7f8b27a144
David Elvira Martin <delvira-@c3r1s5.42malaga.com>
2022-12-05 15:03:44 +0100

    matrix done

--- FdF/main.c
+++ FdF/main.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: delvira- <delvira-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/05 14:46:36 by delvira-          #+#    #+#             */
+/*   Updated: 2022/12/05 15:03:05 by delvira-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "MLX42/include/MLX42/MLX42.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "libft/libft.h"
 #define WIDTH 256
 #define HEIGHT 256
-#include "libft/libft.h"
 
 int	ft_get_heigh(void)
 {
@@ -43,43 +55,72 @@ int	ft_get_line_width(void)
 	return (x);
 }
 
-int	**ft_matrix(void)
+int	**ft_matrix_fill(int **matrix, int fd)
+{
+	int		i;
+	int		j;
+	int		x;
+	char	*line;
+	char	**linesplited;
+
+	x = 0;
+	j = 0;
+	x = 0;
+	line = ft_get_next_line(fd);
+	while (line)
+	{
+		i = 0;
+		j = 0;
+		linesplited = ft_split(line, ' ');
+		while (linesplited[j])
+		{
+			matrix[x][i] = ft_atoi(linesplited[j]);
+			j++;
+			i++;
+		}
+		line = ft_get_next_line(fd);
+		x++;
+	}	
+	return (matrix);
+}
+
+int	**ft_matrix(int heigh, int width)
 {
 	int		**matrix;
 	int		fd;
-	char	*line;
-	char	**linesplited;
-	int		i;
-	int		x;
-	int		j;
+	int		malloccounter;
 
-	i = 0;
-	x = 0;
-	j = 0;
-	line = "";
+	malloccounter = 0;
 	fd = open("test.txt", O_RDONLY);
-	matrix = (int **)malloc((ft_get_heigh() * ft_get_line_width())
-			* sizeof(int));
-	while (line)
+	matrix = (int **)malloc(heigh * sizeof(int *));
+	if (!matrix)
+		return (matrix);
+	while (malloccounter < heigh)
 	{
-	i = 0;
-	j = 0;
-	line = ft_get_next_line(fd);
-	linesplited = ft_split(line, ' ');
-		while (linesplited[j])
-		{
-		matrix[x][i] = ft_atoi(linesplited[j]);
-		i++;
-		j++;
-		}
-	x++;
+		matrix[malloccounter] = (int *)malloc(width * sizeof(int));
+		malloccounter++;
 	}
+	matrix = ft_matrix_fill(matrix, fd);
 	close(fd);
 	return (matrix);
 }
 
 int	main(void)
 {
-	ft_matrix();
-	system("leaks -q ./fdf");
+	int	**matrix;
+	// int	heigh;
+	// int	width;
+
+	// heigh = ft_get_heigh();
+	// width = ft_get_line_width();
+	matrix = ft_matrix(ft_get_heigh(), ft_get_line_width());
+// 	int row, columns;
+// for (row=0; row<heigh; row++)
+// {
+//     for(columns=0; columns<width; columns++)
+//     {
+//         printf("%i ", matrix[row][columns]);
+//     }
+//     printf("\n");
+// }
 }
